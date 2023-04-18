d69648b172b8783853858ac41ed89b87352a5d06
Ben <BOnyechi@its.jnj.com>
2023-04-18 19:10:37 -0400

    save state for this

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::{HashMap, HashSet};
+// use std::fmt::format;
 use std::fs::{self, File};
 use std::io::Write;
+use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, Hash, PartialEq, Clone)]
 enum PackageType {
     BioConductor,
     DevTools,
@@ -10,80 +13,78 @@ enum PackageType {
     Unique,
 }
 
-#[derive(Debug)]
-struct RPackage {
-    package_name: String,
-    package_type: PackageType,
+impl PackageType {
+    fn as_str(&self) -> &'static str {
+        match self {
+            PackageType::BioConductor => "BiocManager::install",
+            PackageType::DevTools => "devtools::install_github",
+            PackageType::Remote => "remotes::install_github",
+            PackageType::Normal => "install.packages",
+            PackageType::Unique => "",
+        }
+    }
 }
 
-fn process_r_pkgs(script: Vec<String>) -> Vec<RPackage> {
-    let r_packages: Vec<RPackage> = script
+impl FromStr for PackageType {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<PackageType, Self::Err> {
+        match input {
+            x if x.contains("install.packages") => Ok(PackageType::BioConductor),
+            x if x.contains("BiocManager") => Ok(PackageType::DevTools),
+            x if x.contains("devtools") => Ok(PackageType::Remote),
+            x if x.contains("remotes") => Ok(PackageType::Normal),
+            _ => Ok(PackageType::Unique),
+        }
+    }
+}
+
+// #[derive(Debug)]
+// struct RPackage {
+//     package_name: String,
+//     package_type: PackageType,
+// }
+
+fn process_r_pkgs(script: Vec<String>) -> HashMap<PackageType, HashSet<String>> {
+    let r_packages: HashMap<PackageType, HashSet<String>> = script
         .iter()
         .filter(|x| x.contains("R"))
         .map(|item| {
             let split_content: Vec<_> = item.split(|c: char| c == '(' || c == ')').collect();
             let parsed_item = split_content.get(1).unwrap_or(&"Not Available");
             let matched_item = parsed_item.replace("'", "");
-            let package_type = match item {
-                x if x.contains("install.packages") => PackageType::Normal,
-                x if x.contains("BiocManager") => PackageType::BioConductor,
-                x if x.contains("devtools") => PackageType::DevTools,
-                x if x.contains("remotes") => PackageType::Remote,
-                _ => PackageType::Unique,
-            };
-            RPackage {
-                package_name: matched_item,
-                package_type,
-            }
+            let package_type = PackageType::from_str(item).unwrap();
+            (package_type, matched_item)
         })
-        .collect();
+        .fold(HashMap::new(), |mut acc, (key, val)| {
+            acc.entry(key).or_insert(HashSet::new()).insert(val);
+            acc
+        });
     return r_packages;
 }
 
-fn write_r_pkgs(pkgs: &Vec<RPackage>) {
+fn write_r_pkgs(pkgs: HashMap<PackageType, HashSet<String>>) {
     let install_method = "RUN R --no-save -e";
     let path = "Dockerfile";
     let mut output = File::create(path).expect("couldn't create the file");
-    let instructions = pkgs.iter().map(|pkg| {
-        match pkg {
-            RPackage {
-                package_name,
-                package_type: PackageType::Remote,
-            } => {
-                return format!(
-                    "{} remotes::install_github('{}')\n",
-                    install_method, package_name
-                )
-            }
-            RPackage {
-                package_name,
-                package_type: PackageType::BioConductor,
-            } => {
-                return format!(
-                    "{} BiocManager::install('{}')\n",
-                    install_method, package_name
-                )
-            }
-            RPackage {
-                package_name,
-                package_type: PackageType::DevTools,
-            } => {
-                return format!(
-                    "{} devtools::install_github('{}')\n",
-                    install_method, package_name
-                )
-            }
-            RPackage {
-                package_name,
-                package_type: PackageType::Normal,
-            } => {
-                return format!("{} install.packages('{}')\n", install_method, package_name)
-            }
-            _ => "".to_string(),
+    for (pkg_type, pkg_map) in pkgs.iter() {
+        let mut chunks = pkg_map.iter().peekable();
+        while chunks.peek().is_some() {
+            let chunk_vec: Vec<&String> = chunks.by_ref().take(5).collect();
+            let pkg_name = chunk_vec
+                .iter()
+                .map(|&x| format!("'{}'", x))
+                .collect::<Vec<String>>()
+                .join(", ");
+            write!(
+                output,
+                "{} \"{}(c({}))\"\n",
+                install_method,
+                pkg_type.as_str(),
+                pkg_name
+            )
+            .expect("Package failed to write");
         }
-    }).collect::<Vec<String>>();
-    for row in instructions.iter() {
-        write!(output,"{}", row).expect("Package failed to write");
     }
 }
 
@@ -100,5 +101,6 @@ fn main() {
         })
         .collect();
     let r_pkgs = process_r_pkgs(content);
-    write_r_pkgs(&r_pkgs);
+    println!("{:?}", r_pkgs);
+    write_r_pkgs(r_pkgs);
 }
