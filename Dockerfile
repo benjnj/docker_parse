98858abf5769068cb25676864bad761a85174551
Ben <BOnyechi@its.jnj.com>
2023-04-20 18:30:25 -0400

    updated to use walkdir instead

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-RUN R --no-save -e "devtools::install_github(c('c'))"
-RUN R --no-save -e "BiocManager::install(c(''))"
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 // use std::fmt::format;
 use std::env;
 use std::fs::{self, File};
 use std::io::Write;
 use std::str::FromStr;
+
+use walkdir::WalkDir;
 
 #[derive(Debug, Eq, Hash, PartialEq, Clone)]
 enum PackageType {
@@ -40,41 +42,36 @@ impl FromStr for PackageType {
     }
 }
 
-// #[derive(Debug)]
-// struct RPackage {
-//     package_name: String,
-//     package_type: PackageType,
-// }
-
-fn process_r_pkgs(script: Vec<String>) -> HashMap<PackageType, HashSet<String>> {
-    let r_packages: HashMap<PackageType, HashSet<String>> = script
+fn process_r_pkgs(script: Vec<String>) -> HashMap<PackageType, Vec<String>> {
+    let r_packages: HashMap<PackageType, Vec<String>> = script
         .iter()
         .filter(|x| x.contains("R"))
         .map(|item| {
             let split_content: Vec<_> = item.split(|c: char| c == '(' || c == ')').collect();
-            let parsed_item = split_content.get(1).unwrap_or(&"Not Available");
+            let parsed_item = split_content.get(1).unwrap_or(&"");
             let matched_item = parsed_item.replace("'", "");
             let package_type = PackageType::from_str(item).unwrap();
             (package_type, matched_item)
         })
         .fold(HashMap::new(), |mut acc, (key, val)| {
-            acc.entry(key).or_insert(HashSet::new()).insert(val);
+            let item = acc.entry(key).or_insert(Vec::new());
+            if !item.contains(&val) {
+                item.push(val)
+            };
             acc
         });
     return r_packages;
 }
 
-fn write_r_pkgs(pkgs: HashMap<PackageType, HashSet<String>>) {
+fn write_r_pkgs(pkgs: HashMap<PackageType, Vec<String>>) {
     let install_method = "RUN R --no-save -e";
     let path = "Dockerfile";
     let mut output = File::create(path).expect("couldn't create the file");
     for (pkg_type, pkg_map) in pkgs.iter() {
-        let mut chunks = pkg_map.iter().peekable();
-        while chunks.peek().is_some() {
-            let chunk_vec: Vec<&String> = chunks.by_ref().take(10).collect();
-            let pkg_name = chunk_vec
+        for chunk in pkg_map.chunks(10) {
+            let pkg_name = chunk
                 .iter()
-                .map(|&x| format!("'{}'", x))
+                .map(|x| format!("'{}'", x))
                 .collect::<Vec<String>>()
                 .join(", ");
             write!(
@@ -93,33 +90,29 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let default_path = ".".to_string();
     let dir_path = args.get(1).unwrap_or(&default_path);
-    let filenames: Vec<_> = fs::read_dir(dir_path)
-        .unwrap()
-        .filter_map(|entry| {
-            let path = entry.unwrap().path();
-            if path.is_file() && path.extension().unwrap_or_default() == "sh" {
-                Some(path)
-            } else {
-                None
-            }
-        })
-        .map(|path| fs::read_to_string(path).unwrap_or_default())
+    let walk = WalkDir::new(dir_path).max_depth(2);
+    let filenames: Vec<_> = walk
+        .into_iter()
+        .filter(|f| f.is_ok())
+        .map(|f| f.unwrap())
         .collect();
     let data: Vec<_> = filenames
         .iter()
-        .flat_map(|line| line.split("\n"))
-        .map(|file| file.split_whitespace().collect::<Vec<_>>().join(" "))
+        .map(|p| p.path())
+        .filter(|p| p.is_file() && p.extension().unwrap_or_default() == "sh")
+        .map(|content| fs::read_to_string(content).unwrap_or_default())
         .collect();
-    let content: Vec<String> = data
+    let parsed_data: Vec<_> = data
         .iter()
-        .filter(|s| s.contains("RUN"))
+        .flat_map(|l| l.split("\n"))
+        .filter(|s| !s.is_empty() && s.contains("RUN"))
         .map(|x| {
-            x.split_whitespace()
+            x.replace("&&", "").split_whitespace()
                 .filter(|&x| x != "RUN")
                 .collect::<Vec<&str>>()
                 .join(" ")
         })
         .collect();
-    let r_pkgs = process_r_pkgs(content);
+    let r_pkgs = process_r_pkgs(parsed_data);
     write_r_pkgs(r_pkgs);
 }
